0b9fbc9c772174ac74f1dcf0f3a96d8a4dc44919
Kushal Patel <kushal300603@gmail.com>
2024-04-30 11:06:06 +0530

    BadgerDB and CLI added

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -16,10 +16,37 @@ type BlockChain struct {
 	Database *badger.DB
 }
 
+type BlockChainIterator struct {
+	CurrentHash []byte
+	Database    *badger.DB
+}
+
 func (chain *BlockChain) AddBlock(data string) {
-	// prevBlock := chain.Blocks[len(chain.Blocks)-1]
-	// newBlock := CreateBlock(data, prevBlock.Hash)
-	// chain.Blocks = append(chain.Blocks, newBlock)
+	var lastHash []byte
+
+	err := chain.Database.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte("lh"))
+		Handle(err)
+		err = item.Value(func(val []byte) error {
+			lastHash = val
+			return nil
+		})
+		return err
+	})
+	Handle(err)
+
+	newBlock := CreateBlock(data, lastHash)
+
+	err = chain.Database.Update(func(txn *badger.Txn) error {
+		err := txn.Set(newBlock.Hash, newBlock.Serialize())
+		Handle(err)
+		err = txn.Set([]byte("lh"), newBlock.Hash)
+
+		chain.LastHash = newBlock.Hash
+
+		return err
+	})
+	Handle(err)
 }
 
 func InitBlockchain() *BlockChain {
@@ -56,4 +83,30 @@ func InitBlockchain() *BlockChain {
 
 	blockchain := &BlockChain{lastHash, db}
 	return blockchain
+}
+
+func (chain *BlockChain) Iterator() *BlockChainIterator {
+	iter := &BlockChainIterator{chain.LastHash, chain.Database}
+
+	return iter
+}
+
+func (iter *BlockChainIterator) Next() *Block {
+	var block *Block
+
+	err := iter.Database.View(func(txn *badger.Txn) error {
+		item, err := txn.Get(iter.CurrentHash)
+		Handle(err)
+		var encodedBlock []byte
+		err = item.Value(func(val []byte) error {
+			encodedBlock = val
+			return nil
+		})
+		block = Deserialize(encodedBlock)
+		return err
+	})
+	Handle(err)
+	iter.CurrentHash = block.PrevHash
+
+	return block
 }
